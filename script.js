4d781bc89c2a758951c0b99b5fda5200ac27db2a
Sumeet Birendra Singh <sumeetbsingh2002@gmail.com>
2023-07-08 18:20:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "<span style='color: white;'>Thanks for your submission!</span>";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "<span style='color: white;'>Oops! There was a problem submitting your form</span>"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "<span style='color: white;'>Oops! There was a problem submitting your form</span>"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
